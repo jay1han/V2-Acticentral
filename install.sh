e849a7bfcd2ac769a3883830d8581f06336d3983
Jay Han <jayhan.name@gmail.com>
2023-05-16 22:27:56 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+mkdir /etc/actimetre
+mkdir /media/actimetre
+mkdir /media/actimetre/Data
+mkdir /media/actimetre/Repo
+
+cp actimetre.conf /etc/actimetre
+cp clear2.sh /etc/actimetre
+cp cgi-bin/acticentral.py /var/www/cgi-bin/acticentral.py
+cp html/index.html error.html /var/www/html/
+
+cd /etc/actimetre
+chown www-data:www-data . actimetre.conf
+chmod 777 . clear2.sh
+chmod 666 actimetre.conf
+
+cd /media/actimetre
+chown www-data:www-data . Data Repo
+chmod 777 . Data Repo
+
+cd /var/www/html
+chown www-data:www-data index.html error.html
+chmod 775 index.html
+
+cd /var/www/cgi-bin
+chown www-data:www-data acticentral.py
+chmod 775 acticentral.py
+
+echo > /etc/actimetre/server.log
+echo > /etc/actimetre/central.log
+echo > /etc/actimetre/acticentral.lock
+rm -f /media/actimetre/Data/*
+rm -f /media/actimetre/Repo/*
+echo {} > /etc/actimetre/registry.data
+echo {} > /etc/actimetre/actiservers.data
+echo {} > /etc/actimetre/actimetres.data
+echo {} > /etc/actimetre/self.data
+echo {} > /etc/actimetre/meta.data
+rm -f /etc/actimetre/acticentral.pid
