9354f5591d32c64141e19d68fa0248dca0e0f853
Jay Han <127133985+jay1han@users.noreply.github.com>
2023-05-18 20:38:51 +0200

    Separate clear script server and central

---
+++ clearcentral.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+echo > /etc/actimetre/central.log
+echo > /etc/actimetre/acticentral.lock
+echo {} > /etc/actimetre/registry.data
+echo {} > /etc/actimetre/actiservers.data
+echo {} > /etc/actimetre/actimetres.data
+echo {} > /etc/actimetre/meta.data
