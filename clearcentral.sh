31471d79ebd19c62bd39908ffadd2e35e967e231
Jay Han <127133985+jay1han@users.noreply.github.com>
2023-06-03 18:43:04 +0200

    Update clearcentral.sh

--- clearcentral.sh
+++ clearcentral.sh
@@ -4,6 +4,7 @@ echo > /etc/actimetre/central.log
 echo > /etc/actimetre/acticentral.lock
 echo {} > /etc/actimetre/actiservers.data
 echo {} > /etc/actimetre/actimetres.data
+chmod 666 *.data *.log
 rm -f /etc/actimetre/history/Actim*.hist
 
 ls -lRA
