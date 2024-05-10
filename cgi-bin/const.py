c2a864f4ff6069c74dafb224b34272d9702577ea
Jay Han <jayhan.name@gmail.com>
2024-05-10 22:14:12 +0200

    v406

--- cgi-bin/const.py
+++ cgi-bin/const.py
@@ -4,7 +4,7 @@ import os, json, subprocess
 from datetime import datetime, timedelta, timezone
 from yattag import Doc
 
-VERSION_STR     = "v405"
+VERSION_STR     = "v406"
 FILE_ROOT       = "/etc/actimetre"
 WWW_ROOT        = "/var/www"
 
