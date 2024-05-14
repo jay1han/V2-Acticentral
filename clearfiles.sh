355b546fcad555e87be6feab455b0694f6922770
Jay Han <jayhan.name@gmail.com>
2024-05-14 20:04:32 +0200

    v445

--- clearfiles.sh
+++ clearfiles.sh
@@ -5,3 +5,4 @@ rm /var/www/html/actimetre/actim*.html
 rm /var/www/html/project/project*.html
 rm /var/www/html/actiserver/server*.html
 rm /var/www/html/images/actim*.svg
+rm /var/www/html/acticentral.html
